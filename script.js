61c14589be5b81bf30f1e414616f5cb8b99842a4
phoenixwright-creator <85300844+phoenixwright-creator@users.noreply.github.com>
2021-11-12 15:16:43 +0100

    styled the workout app, finished the timer function to reset to default when reset button is clicked, or continue either way

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-
 const defaultState = {
     breakLength: 5,
-    sessionLength: 5,
+    sessionLength: 25,
     breakTime: 5,
-    sessionTime: 5,
+    sessionTime: 25,
     display: 'session',
-    running: ''
-};
-
+    reset: true
+}
 const BREAKINCREMENT = 'BREAKINCREMENT';
 const BREAKDECREMENT = 'BREAKDECREMENT';
 const SESSIONINCREMENT = 'SESSIONINCREMENT';
@@ -16,7 +14,6 @@ const STARTSESSION = 'STARTSESSION';
 const STARTBREAK = 'STARTBREAK';
 const STOP = 'STOP';
 const RESET = 'RESET';
-
 const reducer = (state = defaultState, action) => {
     const newState = Object.assign({}, state);
     switch(action.type){
@@ -57,11 +54,10 @@ const reducer = (state = defaultState, action) => {
                 return state;
             }
         case STARTSESSION:
-            newState.running = 'on';
+            newState.reset = false;
             newState.sessionTime--;
             return newState;
         case STARTBREAK:
-            newState.display = 'break';
             newState.breakTime--;
             return newState;
         case RESET:
@@ -70,89 +66,87 @@ const reducer = (state = defaultState, action) => {
             return state;
     }
 };
-
 const store = Redux.createStore(reducer);
-
 Window.store = store;
-
 function breakDecrement(){
     store.dispatch({
         type: BREAKDECREMENT
     });
     render();
 }
-
 function breakIncrement(){
     store.dispatch({
         type: BREAKINCREMENT
     });
     render();
 }
-
 function sessionDecrement(){
     store.dispatch({
         type: SESSIONDECREMENT
     });
     render();
 }
-
 function sessionIncrement(){
     store.dispatch({
         type: SESSIONINCREMENT
     });
     render();
 }
-
 function startStop(){
-    let newState;
+    let newState = store.getState();
+    newState.reset = false;
+    newState.breakTime = newState.breakLength + 1;
     let timer = setInterval(() => {
-            store.dispatch({
-                type: STARTSESSION
-            });
-            render();
-            let newState = store.getState();
-            if(newState.sessionTime === 0){
+            if(newState.reset === true){
                 clearInterval(timer);
-                console.log(store.getState());
-                timer = setInterval(() => {
-                    store.dispatch({
-                        type: STARTBREAK
-                    });
-                    render();
-                    newState = store.getState();
-                    if(newState.breakTime === 0){
-                        clearInterval(timer);
-                        console.log(store.getState());
-                    }
-                }, 1000);
+            }
+            else if(newState.display === 'session'){
+                store.dispatch({
+                    type: STARTSESSION
+                });
+                render();
+                newState = store.getState();
+                if(newState.sessionTime === 0){
+                newState.display = 'break';
+                newState.sessionTime = newState.sessionLength + 1;
+                }
+            }
+            else if(newState.display === 'break'){
+                store.dispatch({
+                    type: STARTBREAK
+                });
+                render();
+                newState = store.getState();
+                if(newState.breakTime === 0){
+                    newState.display = 'session';
+                    newState.breakTime = newState.breakLength + 1;
+                }
             }
     }, 1000);
-    
 }
-
 function reset(){
-    location.reload();
+    let newState = store.getState();
+    newState.reset = true;
     store.dispatch({
         type: RESET
     });
     render();
 }
-
 function render(){
     const state = store.getState();
     const breakLength = document.getElementById("break-length");
     breakLength.innerHTML = state.breakLength;
     const sessionLength = document.getElementById("session-length");
     sessionLength.innerHTML = state.sessionLength;
+    const timerLabel = document.getElementById("timer-label");
     const time = document.getElementById("time-left");
     if(state.display === 'session'){
+        timerLabel.innerHTML = 'Session';
         time.innerHTML = state.sessionTime + ":00";
     }
     else if(state.display === 'break'){
+        timerLabel.innerHTML = 'Break';
         time.innerHTML = state.breakTime + ":00";
     }
-    
 }
-
 render();
-
