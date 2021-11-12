7da51b1fd56b67362e7cc30d47f2839e5b9ac7b8
phoenixwright-creator <85300844+phoenixwright-creator@users.noreply.github.com>
2021-11-12 13:35:14 +0100

    added some functionality to the start timer function to decrement the time left each second and display the break time when the session time is over

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 
 const defaultState = {
     breakLength: 5,
-    sessionLength: 25,
+    sessionLength: 5,
     breakTime: 5,
-    sessionTime: 25,
+    sessionTime: 5,
     display: 'session',
     running: ''
 };
+
 const BREAKINCREMENT = 'BREAKINCREMENT';
 const BREAKDECREMENT = 'BREAKDECREMENT';
 const SESSIONINCREMENT = 'SESSIONINCREMENT';
 const SESSIONDECREMENT = 'SESSIONDECREMENT';
-const STARTSTOP = 'STARTSTOP';
+const STARTSESSION = 'STARTSESSION';
+const STARTBREAK = 'STARTBREAK';
+const STOP = 'STOP';
 const RESET = 'RESET';
+
 const reducer = (state = defaultState, action) => {
     const newState = Object.assign({}, state);
     switch(action.type){
         case BREAKINCREMENT:
             if(newState.breakLength < 60){
                 newState.breakLength++;
+                newState.breakTime = newState.breakLength;
                 return newState;
             }
             else{
@@ -27,6 +32,7 @@ const reducer = (state = defaultState, action) => {
         case BREAKDECREMENT:
             if(newState.breakLength > 1){
                 newState.breakLength--;
+                newState.breakTime = newState.breakLength;
                 return newState;
             }
             else{
@@ -35,6 +41,7 @@ const reducer = (state = defaultState, action) => {
         case SESSIONINCREMENT:
             if(newState.sessionLength < 60){
                 newState.sessionLength++;
+                newState.sessionTime = newState.sessionLength;
                 return newState;
             }
             else{
@@ -43,20 +50,20 @@ const reducer = (state = defaultState, action) => {
         case SESSIONDECREMENT:
             if(newState.sessionLength > 1){
                 newState.sessionLength--;
+                newState.sessionTime = newState.sessionLength;
                 return newState;
             }
             else{
                 return state;
             }
-        case STARTSTOP:
-            if(newState.sessionTime > 0){
-                newState.running = 'start';
-                newState.sessionTime--;
-                return newState;
-            }
-            else{
-                clearInterval(timer);
-            }
+        case STARTSESSION:
+            newState.running = 'on';
+            newState.sessionTime--;
+            return newState;
+        case STARTBREAK:
+            newState.display = 'break';
+            newState.breakTime--;
+            return newState;
         case RESET:
             return defaultState;
         default:
@@ -74,35 +81,57 @@ function breakDecrement(){
     });
     render();
 }
+
 function breakIncrement(){
     store.dispatch({
         type: BREAKINCREMENT
     });
     render();
 }
+
 function sessionDecrement(){
     store.dispatch({
         type: SESSIONDECREMENT
     });
     render();
 }
+
 function sessionIncrement(){
     store.dispatch({
         type: SESSIONINCREMENT
     });
     render();
 }
-let timer = setInterval(() => {
-    store.dispatch({
-        type: STARTSTOP
-    });
-    render();
-}, 1000);
+
 function startStop(){
-    timer;
-};
+    let newState;
+    let timer = setInterval(() => {
+            store.dispatch({
+                type: STARTSESSION
+            });
+            render();
+            let newState = store.getState();
+            if(newState.sessionTime === 0){
+                clearInterval(timer);
+                console.log(store.getState());
+                timer = setInterval(() => {
+                    store.dispatch({
+                        type: STARTBREAK
+                    });
+                    render();
+                    newState = store.getState();
+                    if(newState.breakTime === 0){
+                        clearInterval(timer);
+                        console.log(store.getState());
+                    }
+                }, 1000);
+            }
+    }, 1000);
+    
+}
 
 function reset(){
+    location.reload();
     store.dispatch({
         type: RESET
     });
@@ -116,7 +145,14 @@ function render(){
     const sessionLength = document.getElementById("session-length");
     sessionLength.innerHTML = state.sessionLength;
     const time = document.getElementById("time-left");
-    time.innerHTML = state.sessionTime + ":00";
+    if(state.display === 'session'){
+        time.innerHTML = state.sessionTime + ":00";
+    }
+    else if(state.display === 'break'){
+        time.innerHTML = state.breakTime + ":00";
+    }
+    
 }
 
 render();
+
